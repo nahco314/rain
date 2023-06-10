155d7579c6fdc7ea2f74fd89be78bf3c343d3361
nahco314 <nahco3_ta@yahoo.co.jp>
2023-06-11 01:42:25 +0900

    fix: fix some bug about C++ tokenize

--- src/rain/cpp/tokenize.py
+++ src/rain/cpp/tokenize.py
@@ -1,22 +1,24 @@
 import re
+import secrets
 
 
 class Tokenizer:
-    preprocessor_directive = re.compile(r"^#.*\n", re.MULTILINE)
+    preprocessor_directive = re.compile(r"^#.*$", re.MULTILINE)
+    comment = re.compile(r"(//[^\n]*?$)|(/\*.*?\*/)", re.DOTALL | re.MULTILINE)
 
     name = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")  # keyword, identifier
+
+    pointfloat = re.compile(r"\d('?\d)*\.(\d('?\d)*)?([eE]-?\d('?\d)*)?[fFlL]?")
+    intfloat = re.compile(r"\d('?\d)*[eE]-?\d('?\d)*[fFlL]?")
 
     bin_int = re.compile(r"0[bB][01]('?[01])*[uU]?[lL]{0,2}")
     hex_int = re.compile(r"0[xX][0-9a-fA-F]('?[0-9a-fA-F])*[uU]?[lL]{0,2}")
     oct_int = re.compile(r"0('?[0-7])*[uU]?[lL]{0,2}")
     int = re.compile(r"\d('?\d)*[uU]?[lL]{0,2}")
 
-    pointfloat = re.compile(r"\d('?\d)*\.(\d('?\d)*)?([eE]-?\d('?\d)*)?[fFlL]")
-    intfloat = re.compile(r"\d('?\d)*[eE]-?\d('?\d)*[fFlL]")
-
     bool = re.compile(r"(true|false)")
 
-    char = re.compile(r"(u8|L|u|U)?'.'")
+    char = re.compile(r"(u8|L|u|U)?'(.|\\.)'")
     string = re.compile(r"(u8|L|u|U)?\".*?\"s?")
     raw_string = re.compile(r"(u8|L|u|U)?\"\(.*?\)\"s?")
 
@@ -40,7 +42,8 @@ class Tokenizer:
         "::",
         ".*",
         "->*",
-        "?:",
+        "?",
+        ":",
         "*=",
         "/=",
         "%=",
@@ -52,6 +55,7 @@ class Tokenizer:
         "|=",
         "^=",
         ",",
+        "...",
         ".",
         "->",
         "[",
@@ -61,6 +65,7 @@ class Tokenizer:
         "++",
         "--",
         "~",
+        "!=",
         "!",
         "-",
         "+",
@@ -74,7 +79,6 @@ class Tokenizer:
         "<",
         ">",
         "==",
-        "!=",
         "&&",
         "||",
         "&",
@@ -91,13 +95,14 @@ class Tokenizer:
 
     rules = (
         preprocessor_directive,
+        comment,
         name,
+        pointfloat,
+        intfloat,
         bin_int,
         hex_int,
         oct_int,
         int,
-        pointfloat,
-        intfloat,
         bool,
         char,
         string,
@@ -108,19 +113,50 @@ class Tokenizer:
         ignore,
     )
 
+    func_macro_def = re.compile(r"#define +([a-zA-Z_][a-zA-Z0-9_]*)\(.*\) +.*$")
+
     def tokenize(self, source: str) -> list[str]:
         res = []
 
+        func_macros = ["assert"]
+
         now = 0
+        macro_par_level = -1
         while now < len(source):
             for rule in self.rules:
                 m = rule.match(source, now)
-                if m:
-                    if rule != self.ignore:
-                        res.append(m.group(0))
+                if not m:
+                    continue
 
-                    now = m.end()
-                    break
+                fmm = self.func_macro_def.match(m.group(0))
+                if fmm:
+                    func_macros.append(fmm.group(1))
+                    res.append(m.group(0))
+
+                elif macro_par_level != -1:
+                    if m.group(0) == "(":
+                        macro_par_level += 1
+                    elif m.group(0) == ")":
+                        macro_par_level -= 1
+
+                    res[-1] += m.group(0)
+
+                    if macro_par_level == 0:
+                        macro_par_level = -1
+
+                elif rule == self.name and m.group(0) in func_macros:
+                    macro_par_level = 0
+                    res.append(m.group(0))
+
+                elif rule != self.ignore:
+                    res.append(m.group(0))
+
+                else:
+                    assert rule == self.ignore
+
+                now = m.end()
+                break
+
             else:
                 raise Exception(f"tokenize failed at {now}, {res}")
 
